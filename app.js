91bdcd82ef72515ae7581fcec98a1eebd8930578
Jovan Derebanoski <jd7@students.kiron.ngo>
2019-12-10 02:52:20 +0100

    RESTful : Error hadnlers added

--- app.js
+++ app.js
@@ -1,12 +1,30 @@
 const express = require('express');
 const app     = express();
+const morgan  = require('morgan');
 
+const productRoutes = require('./api/routes/products');
+const orderRoutes   = require('./api/routes/orders');
 
-app.use(( req, res, next) => {
-    res.status(200).json({
-        message: 'It works!'
+app.use(morgan('dev'));
+
+// Routes which should handle request
+app.use('/products', productRoutes);
+app.use('/orders'  , orderRoutes);
+
+// we want acces every request that  reaches / line
+app.use(( req, res, next ) => {         
+    const error = new Error('Not found');
+    error.status = 404  ;       // custom 404 handler
+    next(error); 
+});
+
+app.use((error, req, res, next) => { // other kinds of errors
+    res.status(error.status || 500);
+    res.json({
+        error : {
+            message : error.message
+        }
     });
 });
 
-
-module.exports = app
+module.exports = app;
